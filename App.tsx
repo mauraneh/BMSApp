d8c0edbf5070ec557ff1a5106be99ea14474e95c
Melina Mitterrand <melina.mitterrand08@gmail.com>
2023-11-28 16:35:36 +0100

    spotify23

--- App.tsx
+++ App.tsx
@@ -1,28 +1,29 @@
-import * as React from 'react';
-import { useState } from 'react';
-import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
+import * as React from "react";
+import { useState } from "react";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
 
 // Pages
-import Splash from './Pages/Splash';
-import { BottomTabs } from './Components/BottomTabs';
-import FontLoader from './Components/Fonts';
+import Splash from "./Pages/Splash";
+import { BottomTabs } from "./Components/bottomTabs";
+import FontLoader from "./Components/fonts";
 import { AuthProvider } from "./src/AuthContext";
 import AuthSpotify from "./src/AuthSpotify";
 
-
-
 export default function App() {
   const [isLoading, setIsLoading] = useState(true);
-   return (
+  return (
     <AuthProvider>
       <AuthSpotify />
-      {isLoading ? <Splash setIsLoading={setIsLoading} /> :   <FontLoader>
-    <NavigationContainer>
-      <BottomTabs />
-    </NavigationContainer>
-  </FontLoader>}
+      {isLoading ? (
+        <Splash setIsLoading={setIsLoading} />
+      ) : (
+        <FontLoader>
+          <NavigationContainer>
+            <BottomTabs />
+          </NavigationContainer>
+        </FontLoader>
+      )}
     </AuthProvider>
   );
-
 }
