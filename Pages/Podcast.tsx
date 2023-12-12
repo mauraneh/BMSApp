46f9201693b7d241991d7a8e10e986f6a675c338
Melina Mitterrand <melina.mitterrand08@gmail.com>
2023-12-12 10:04:08 +0100

    playlist debug

--- Pages/Podcast.tsx
+++ Pages/Podcast.tsx
@@ -1,23 +1,39 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { View, Text, StyleSheet, TouchableOpacity, Image, Dimensions } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  Dimensions,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useAuth } from "../src/AuthContext";
 import { StatusBar } from "react-native";
 import { ScrollView } from "react-native";
 import { ImageBackground } from "react-native";
-import { AntDesign } from '@expo/vector-icons';
+import { AntDesign } from "@expo/vector-icons";
 import GreenImage from "../assets/images/layerGreen.png";
 
 function millisToMinutesAndSeconds(millis) {
   const minutes = Math.floor(millis / 60000);
   const seconds = ((millis % 60000) / 1000).toFixed(0);
-  return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+  return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
 }
 
 const Podcast = () => {
   const { accessToken } = useAuth();
-  const [podcasts, setPodcasts] = useState<{ name: string; id: string; img: string, description: string; duration_ms: number; release_date: Date}[]>([]);
+  const [podcasts, setPodcasts] = useState<
+    {
+      name: string;
+      id: string;
+      img: string;
+      description: string;
+      duration_ms: number;
+      release_date: Date;
+    }[]
+  >([]);
 
   useEffect(() => {
     const fetchPodcasts = async () => {
@@ -46,40 +62,47 @@ const Podcast = () => {
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView
-          pagingEnabled
-          showsHorizontalScrollIndicator={false}
-          scrollEventThrottle={16}
-        >
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        scrollEventThrottle={16}
+      >
         <View>
-        <ImageBackground source={ GreenImage } style={styles.imageTitle}>
-          <Text style={styles.textTitle}>Nos méditations guidées</Text>
+          <ImageBackground source={GreenImage} style={styles.imageTitle}>
+            <Text style={styles.textTitle}>Nos méditations guidées</Text>
           </ImageBackground>
 
-            <View style={styles.cards}>
-              {podcasts.map((podcast) => (
-                <View style={styles.card}>
-                    <TouchableOpacity key={podcast.id}>
-                      <ImageBackground source={{uri: podcast.images[0].url}} resizeMode="cover" style={styles.imgCard}>
-                      <Text style={styles.textPlay}>
+          <View style={styles.cards}>
+            {podcasts.map((podcast) => (
+              <View key={podcast.id} style={styles.card}>
+                <TouchableOpacity key={podcast.id}>
+                  <ImageBackground
+                    source={{ uri: podcast.images[0].url }}
+                    resizeMode="cover"
+                    style={styles.imgCard}
+                  >
+                    <Text style={styles.textPlay}>
                       <AntDesign name="play" size={60} color="white" />
-                      </Text>
-                      </ImageBackground>
-                    </TouchableOpacity>
-                    <View>
-                      <Text style={styles.textNameCard}>{podcast.name}</Text>
-                      <Text style={styles.textCard}>Relaxations et médiations guidées</Text>
-                      <Text style={styles.textDescription}>
-                        {podcast.description}
-                      </Text>
-                      <View style={styles.textInfos}>
-                        <Text style={styles.textInfo}>{millisToMinutesAndSeconds(podcast.duration_ms)}</Text>
-                        <Text style={styles.textInfo}>{podcast.release_date}</Text>
-                      </View>
-                    </View>
+                    </Text>
+                  </ImageBackground>
+                </TouchableOpacity>
+                <View>
+                  <Text style={styles.textNameCard}>{podcast.name}</Text>
+                  <Text style={styles.textCard}>
+                    Relaxations et médiations guidées
+                  </Text>
+                  <Text style={styles.textDescription}>
+                    {podcast.description}
+                  </Text>
+                  <View style={styles.textInfos}>
+                    <Text style={styles.textInfo}>
+                      {millisToMinutesAndSeconds(podcast.duration_ms)}
+                    </Text>
+                    <Text style={styles.textInfo}>{podcast.release_date}</Text>
+                  </View>
                 </View>
-              ))}
-            </View>
-            
+              </View>
+            ))}
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -94,19 +117,19 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   cards: {
-    alignItems: 'center',
+    alignItems: "center",
   },
   card: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'row',
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
     margin: 10,
     padding: 10,
     marginTop: 20,
     marginBottom: 6,
     backgroundColor: "#fff",
     borderRadius: 20,
-    height: 'auto',
+    height: "auto",
     width: 360,
     shadowOffset: { width: -2, height: 3 },
     shadowColor: "#171717",
@@ -116,31 +139,31 @@ const styles = StyleSheet.create({
   textNameCard: {
     padding: 10,
     width: 200,
-    textAlign: 'left',
-    color: '#27432e',
-    fontFamily: 'Alegreya',
+    textAlign: "left",
+    color: "#27432e",
+    fontFamily: "Alegreya",
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   textCard: {
     paddingLeft: 10,
     width: 200,
-    textAlign: 'left',
-    color: '#27432e',
-    fontFamily: 'Alegreya',
+    textAlign: "left",
+    color: "#27432e",
+    fontFamily: "Alegreya",
     fontSize: 12,
   },
   textDescription: {
     padding: 10,
     width: 200,
-    textAlign: 'left',
-    color: '#27432e',
-    fontFamily: 'Alegreya',
+    textAlign: "left",
+    color: "#27432e",
+    fontFamily: "Alegreya",
     fontSize: 10,
   },
   playCard: {
-    textAlign: 'center',
-    textAlignVertical: 'center',
+    textAlign: "center",
+    textAlignVertical: "center",
   },
   imgCard: {
     width: 120,
@@ -149,33 +172,32 @@ const styles = StyleSheet.create({
     shadowColor: "#171717",
     shadowOpacity: 0.5,
     shadowRadius: 1,
-    
   },
   imageTitle: {
-    width: Dimensions.get('window').width,
+    width: Dimensions.get("window").width,
   },
   textTitle: {
-    fontFamily: 'Alegreya',
-    color: 'white',
+    fontFamily: "Alegreya",
+    color: "white",
     fontSize: 30,
     lineHeight: 100,
-    fontWeight: 'bold',
-    textAlign: 'center',
+    fontWeight: "bold",
+    textAlign: "center",
   },
   textPlay: {
     paddingTop: 20,
-    textAlign: 'center',
-    color: '#fff',
+    textAlign: "center",
+    color: "#fff",
     opacity: 0.7,
   },
   textInfos: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
+    flexDirection: "row",
+    justifyContent: "space-around",
   },
   textInfo: {
     width: 90,
-    color: 'grey',
-    fontFamily: 'Alegreya',
+    color: "grey",
+    fontFamily: "Alegreya",
     fontSize: 15,
   },
 });
